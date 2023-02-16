e0d6a9dd3369cca107c74b3966f03480e4e4bbc1
Anouk As <anouk.as@accg.be>
2023-02-16 17:20:21 +0100

    wip test query

---
+++ YogApp.API/Schema/Queries/Query.cs
@@ -0,0 +1,10 @@
+﻿namespace YogApp.API.Schema.Queries;
+
+[ExtendObjectType(OperationTypeNames.Query)]
+public class Query
+{
+    public string TestQuery()
+    {
+        return "this is a query";
+    }
+}
