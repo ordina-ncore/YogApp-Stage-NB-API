a9f13e02e9874cac709704cb4f8e59178c4acb5f
Anouk As <anouk.as@accg.be>
2023-02-16 17:23:23 +0100

    initial

--- YogApp.API/Program.cs
+++ YogApp.API/Program.cs
@@ -3,12 +3,15 @@ using YogApp.API.Schema.Queries;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGraphQLServer()
+    //queries
     .AddQueryType()
         .AddTypeExtension<Query>();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+//app.UseAuthorization();
 
 app.MapGraphQL();
 
